27cc71beaf902b4421b6314df0442c2e966d324c
Barbara <barbaraelisabethb@gmail.com>
2023-06-23 19:36:58 -0400

    Completed Back-End Layer Setup

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,23 @@
 from app import db
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+
+
+    def to_dict(self):
+        return {
+            "board_id": self.board_id,
+            "title": self.title,
+            "owner": self.owner
+        }
+    
+    @classmethod
+    def from_dict(cls, board_data):
+        new_board = Board(
+            title=board_data["title"],
+            owner=board_data["owner"]
+        )
+        return new_board
+    
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,22 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+
+
+    def to_dict(self):
+        return {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count
+        }
+    
+    @classmethod
+    def from_dict(cls, card_data):
+        new_card = Card(
+            message=card_data["message"],
+            likes_count=card_data["likes_count"]
+        )
+        return new_card
