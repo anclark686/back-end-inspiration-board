0ca9b8d9045f50904c702b4fad6461fbc89f612a
anclark686 <anclark686@gmail.com>
2023-06-26 18:34:09 -0400

    adding model imports

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
