56ddd0084e63255080ceed5e2042e4b32717ab54
anclark686 <anclark686@gmail.com>
2023-06-26 18:28:58 -0400

    adding bp

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -2,3 +2,4 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
+board_bp = Blueprint("boards", __name__, url_prefix="/boards")
