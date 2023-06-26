9f4917e839133463c77e4e2002cdec782b619a10
anclark686 <anclark686@gmail.com>
2023-06-26 18:38:55 -0400

    removing my unfinished work

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -5,14 +5,3 @@ from .routes_helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
-
-
-# ADD LIKE TO CARD
-@card_bp.route("/<id>/add_like", methods=["PATCH"])
-def mark_incomplete(id):
-    card = validate_model(Card, id)
-
-    card.completed_at = None
-    db.session.commit()
-
-    return make_response(jsonify({"card": card.to_dict()}), 200)
