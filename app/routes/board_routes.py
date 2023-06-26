527641341772391ffdb449cb8d6fd76cb19a6780
anclark686 <anclark686@gmail.com>
2023-06-26 18:36:46 -0400

    adding route helper

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.board import Board
+from .routes_helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,6 +1,18 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.card import Card
+from .routes_helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+
+# ADD LIKE TO CARD
+@card_bp.route("/<id>/add_like", methods=["PATCH"])
+def mark_incomplete(id):
+    card = validate_model(Card, id)
+
+    card.completed_at = None
+    db.session.commit()
+
+    return make_response(jsonify({"card": card.to_dict()}), 200)
