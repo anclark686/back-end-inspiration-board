221b0dab2d074988857f11ff7ebd27b0e1e6870d
anclark686 <anclark686@gmail.com>
2023-06-26 18:34:22 -0400

    adding model imports2

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
