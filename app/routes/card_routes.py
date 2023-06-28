fb9a6a86862f2c4a3bea321391c25a0061cdbca9
Doris Lin <lmh7026@gmail.com>
2023-06-28 11:11:09 -0400

    create delete card route

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -5,3 +5,11 @@ from .routes_helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+@card_bp.route("/<id>", methods=["DELETE"])
+def delete_one_card(id):
+    card = validate_model(Card, id)
+
+    db.session.delete(card)
+    db.session.commit()
+    return make_response(jsonify({"details": f'Card {card.id} successfully deleted'}), 200)
