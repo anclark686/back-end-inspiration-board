4f53e31f32daf3110e6dbff5bdc42b318769a133
anclark686 <anclark686@gmail.com>
2023-06-27 23:38:39 -0400

    adding add_like route

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -5,3 +5,13 @@ from .routes_helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+# ADD LIKE TO CARD
+@card_bp.route("/<id>/add_like", methods=["PATCH"])
+def add_like(id):
+    card = validate_model(Card, id)
+
+    card.likes_count += 1
+    db.session.commit()
+
+    return make_response(jsonify({"card like count": card.likes_count}), 200)
