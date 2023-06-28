51184fbe8c5e10ed3ca7db40218c224bd025cc68
Barbara <barbaraelisabethb@gmail.com>
2023-06-28 11:20:05 -0700

    create new board route

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
 from .routes_helpers import validate_model
@@ -11,3 +11,17 @@ def get_all_boards():
     boards = Board.query.all()
     boards_response = [board.to_dict() for board in boards]
     return make_response(jsonify(boards_response), 200)
+
+@board_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+
+    if "title" not in request_body or "owner" not in request_body:
+        abort(make_response(jsonify({"details": "Invalid data"}), 400))
+
+    new_board = Board(title=request_body["title"],
+                      owner=request_body["owner"])
+    db.session.add(new_board)
+    db.session.commit()
+
+    return jsonify(new_board.to_dict()), 201
