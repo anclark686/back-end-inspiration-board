0fd846b1eaf113a790a6d1b191444020f8183d24
anclark686 <anclark686@gmail.com>
2023-06-30 15:05:15 -0400

    adding two card routes to board routes, removing them from card_routes

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
+from app.models.card import Card
 from .routes_helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
@@ -24,3 +25,34 @@ def create_board():
     db.session.commit()
 
     return make_response(jsonify(new_board.to_dict()), 201)
+
+
+# GET ALL TASKS FOR GOAL
+@board_bp.route("/<id>/cards", methods=["GET"])
+def get_board_cards(id):
+    board = validate_model(Board, id)
+    # just a list of cards, not all the board data
+    card_response = [card.to_dict() for card in board.cards]
+    return make_response(jsonify(card_response))
+
+# create a route to add new card for selected board
+@board_bp.route("/<id>/cards", methods=["POST"])
+def create_card(id):
+    request_body = request.get_json()
+    request_body["board_id"] = id
+
+    # need to add char count feedback
+    try:
+        new_card = Card.from_dict(request_body)
+    except KeyError:
+        abort(make_response(jsonify({"details": "Invalid data"}), 400))
+
+    if len(new_card["message"]) > 40:
+        abort(make_response(jsonify({
+            "details": "Message should be 40 characters or less."
+            }), 400))
+
+    db.session.add(new_card)
+    db.session.commit()
+
+    return make_response(jsonify(new_card.to_dict()), 201)
--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -23,26 +23,26 @@ def add_like(id):
     card.likes_count += 1
     db.session.commit()
 
-    return make_response(jsonify({"card like count": card.likes_count}), 200)
+    return make_response(jsonify({"card_like_count": card.likes_count}), 200)
 
-# create a route to add new card for selected board
-@card_bp.route("", methods=["POST"])
-def create_card():
-    request_body = request.get_json()
+# # create a route to add new card for selected board
+# @card_bp.route("", methods=["POST"])
+# def create_card():
+#     request_body = request.get_json()
 
-    try:
-        new_card = Card.from_dict(request_body)
-    except KeyError:
-        abort(make_response(jsonify({"details": "Invalid data"}), 400))
+#     try:
+#         new_card = Card.from_dict(request_body)
+#     except KeyError:
+#         abort(make_response(jsonify({"details": "Invalid data"}), 400))
 
-    db.session.add(new_card)
-    db.session.commit()
+#     db.session.add(new_card)
+#     db.session.commit()
 
-    return make_response(jsonify(new_card.to_dict()), 201)
+#     return make_response(jsonify(new_card.to_dict()), 201)
 
-# create a route to view all cards for selected board
-@card_bp.route("", methods=["GET"])
-def get_all_cards():
-    cards = Card.query.all()
-    cards_response = [card.to_dict() for card in cards]
-    return make_response(jsonify(cards_response), 200)
+# # create a route to view all cards for selected board
+# @card_bp.route("", methods=["GET"])
+# def get_all_cards():
+#     cards = Card.query.all()
+#     cards_response = [card.to_dict() for card in cards]
+#     return make_response(jsonify(cards_response), 200)
