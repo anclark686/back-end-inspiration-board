4026667e1a970584339e1ee53ebb4c6e1a13197a
Doris Lin <lmh7026@gmail.com>
2023-07-04 09:18:24 -0400

    add tests for deleting card

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,7 +13,7 @@ def test_add_like_existing_card(client, one_card):
 
     # Assert
     assert response.status_code == 200
-    assert response_body == {"card like count": likes_count}
+    assert response_body == {"card_like_count": likes_count}
     assert Card.query.get(1).likes_count == likes_count
 
 def test_add_like_missing_card(client):
@@ -28,3 +28,25 @@ def test_add_like_missing_card(client):
     assert response_body == {
         "message": "Card with id 1 was not found."
     }
+
+def test_delete_one_card(client, one_card):
+    response = client.delete("/cards/1")
+    response_body = response.get_json()
+    print(response_body)
+
+    assert response.status_code == 200
+    assert response_body == {"details": "Card 1 successfully deleted"}
+
+def test_delete_invalid_card(client, one_card):
+    response = client.delete("/cards/hellothere")
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"message": f"hellothere is invalid"}
+
+def test_delete_nonexisting_card(client, one_card):
+    response = client.delete("/cards/59303594")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": "Card with id 59303594 was not found."}
