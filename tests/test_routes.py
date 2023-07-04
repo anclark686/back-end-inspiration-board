cee41d88731f6ddf1bebf3e84e3d9dc82fab1a57
Doris Lin <lmh7026@gmail.com>
2023-07-04 10:02:36 -0400

    added fixtures for boards and wrote tests for get all boards

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -50,3 +50,53 @@ def test_delete_nonexisting_card(client, one_card):
 
     assert response.status_code == 404
     assert response_body == {"message": "Card with id 59303594 was not found."}
+
+def test_get_all_boards(client, four_boards):
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert len(response_body) == 4
+    assert response_body == [
+        {
+            "board_id": 1,
+            "owner": "Doris",
+            "title": "Board 1"
+        },
+        {
+            "board_id": 2,
+            "owner": "Danqing",
+            "title": "Board 2"
+        },
+        {
+            "board_id": 3,
+            "owner": "Alycia",
+            "title": "Board 3"
+        },
+        {
+            "board_id": 4,
+            "owner": "Barbara",
+            "title": "Board 4"
+        }
+    ]
+
+def test_get_boards_with_cards(client, two_boards_with_cards):
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "board_id": 1,
+            "card_count": 2,
+            "owner": "Cardboard",
+            "title": "Board with Card"
+        },
+        {
+            "board_id": 2,
+            "card_count": 1,
+            "owner": "Inspired",
+            "title": "Board with Cards Too"
+        },
+    ]
