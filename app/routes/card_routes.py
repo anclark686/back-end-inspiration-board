51e7ad6544e0b16e0557cd8eebdca041f64d3eb2
Doris Lin <lmh7026@gmail.com>
2023-07-18 15:22:19 -0400

    get rid of incorrect routes for getting and creating cards

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -24,25 +24,3 @@ def add_like(id):
     db.session.commit()
 
     return make_response(jsonify({"card_like_count": card.likes_count}), 200)
-
-# # create a route to add new card for selected board
-# @card_bp.route("", methods=["POST"])
-# def create_card():
-#     request_body = request.get_json()
-
-#     try:
-#         new_card = Card.from_dict(request_body)
-#     except KeyError:
-#         abort(make_response(jsonify({"details": "Invalid data"}), 400))
-
-#     db.session.add(new_card)
-#     db.session.commit()
-
-#     return make_response(jsonify(new_card.to_dict()), 201)
-
-# # create a route to view all cards for selected board
-# @card_bp.route("", methods=["GET"])
-# def get_all_cards():
-#     cards = Card.query.all()
-#     cards_response = [card.to_dict() for card in cards]
-#     return make_response(jsonify(cards_response), 200)
